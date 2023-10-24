feaa6d9813a4e80b906069df6b1caf17164dbae7
Juan Cubeddu <vcubjuan@amzon.com>
2023-10-23 19:14:50 -0500

    remove server

--- index.ts
+++ index.ts
@@ -1,16 +1,5 @@
 import figlet from "figlet";
 
-const server = Bun.serve({
-    port: 3000,
-    fetch(req) {
-        const body = figlet.textSync("Graph Explorer", "Banner3-D");
-        return new Response(body);
-    },
-
-
-});
-
 const label = figlet.textSync("GraphExp", "3D-ASCII");
-
 console.log(label);
-console.log(`Listening on http://localhost:${server.port} ...`);
+
